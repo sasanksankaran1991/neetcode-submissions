6fdc3e48a1437f988275aecb8ea0d31aee445e46
Sasank Sankaran <36784914+sasanksankaran1991@users.noreply.github.com>
2026-07-06 11:19:09 +0530

    Add: design-linked-list - submission-0

---
+++ Data Structures & Algorithms/design-linked-list/submission-0.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.size:
+            return
+        if index <= 0:
+            self.head = Node(val, self.head)
+        else:
+            curr = self.head
+            for _ in range(index - 1):
+                curr = curr.next
+            curr.next = Node(val, curr.next)
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        if index == 0:
+            self.head = self.head.next
+        else:
+            curr = self.head
+            for _ in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+        self.size -= 1
