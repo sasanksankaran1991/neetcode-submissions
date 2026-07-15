08a18288e88084a963decd829d3102f2e2a94659
Sasank Sankaran <36784914+sasanksankaran1991@users.noreply.github.com>
2026-07-15 14:43:01 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,47 @@
+import math
+import numpy as np
+class Solution:
+    
+    def check_correct(self, piles, k, h):
+        
+        min_val = 0
+        for x in piles:
+            a = math.ceil(x / k)
+            # print(x,'val', k, a)
+            min_val+=a
+
+        if min_val > h:
+            return (1, min_val)
+        elif min_val <= h:
+            return (-1, min_val)
+        # else:
+            # print(0, k, h, min_val)
+            # return (0, min_val)
+
+    
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        low = 1
+        high = 1000000000
+        # for x in piles:
+        #     if x > high:
+        #         high = x
+        
+        min_mid = np.inf
+        max_k = -1
+        while low <= high:
+            mid = (low+high)//2
+            output, min_val = self.check_correct(piles, mid, h)
+            print(low, mid, high, "--", output, min_val, '**', max_k, min_mid)    
+
+            if output > 0:
+                low = mid +1
+            elif output < 0:
+                if (mid < min_mid) & (min_val >= max_k):
+                    min_mid = mid
+                    min_k = min_val
+                high = mid -1
+        
+        return min_mid
+
+
