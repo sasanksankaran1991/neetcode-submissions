f6c947983404ff2e9f566b6172c770ba4b8e6796
Sasank Sankaran <36784914+sasanksankaran1991@users.noreply.github.com>
2026-07-25 21:58:30 +0530

    Add: three-integer-sum - submission-17

---
+++ Data Structures & Algorithms/three-integer-sum/submission-17.py
@@ -0,0 +1,76 @@
+class Solution:
+
+    def sorting(self, arr1, arr2):
+
+        arr = []
+        i = 0
+        j = 0
+
+        while i<=len(arr1) or j<=len(arr2):
+            if (i==len(arr1)) &  (j==len(arr2)):
+                return arr
+            elif (i==len(arr1)):
+                arr = arr+arr2[j:]
+                j=len(arr2)
+            elif (j==len(arr2)):
+                arr = arr+arr1[i:]
+                i=len(arr1)
+            elif arr1[i] < arr2[j]:
+                arr.append(arr1[i])
+                i+=1
+            else:
+                arr.append(arr2[j])
+                j+=1
+            
+
+
+    def spliting(self, nums):
+        # print(nums)
+        if len(nums) == 1:
+            return nums
+
+        l=0
+        r=len(nums)-1
+        m=(l+r)//2
+        # print(l, m , r)
+        arr1 = self.spliting(nums[:m+1])
+        arr2 = self.spliting(nums[m+1:])
+
+        arr = self.sorting(arr1, arr2)
+
+        return arr
+
+
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        arr = self.spliting(nums)
+        # arr = sorted(nums)
+        # print(arr)
+
+        final_result = []
+        for i in range(len(arr)):
+            if arr[i] > 0:
+                pass
+            if i>0 and arr[i]==arr[i-1]:
+                continue
+            j=i+1
+            k=len(arr)-1
+            while j < k:
+                target = -arr[i]
+                # print(i, j, k, target, arr[j]+arr[k])
+                if arr[j]+arr[k] < target:
+                    j+=1
+                elif arr[j]+arr[k] > target:
+                    k-=1
+                else:
+                    final_result.append([arr[i], arr[j], arr[k]])
+                    # print('match')
+                    j+=1
+                    k-=1
+                    while j<k and arr[j] == arr[j-1]:
+                        j+=1
+                    while j<k and arr[k] == arr[k+1]:
+                        k-=1
+
+        return final_result
